a61011778c4731007bbd146a1f7140e79eec53de
nkru-u <natali_kim666@mail.ru>
2023-11-26 14:26:42 +0900

    1

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 align="left">Mysic Player</a> 
+<img height="32"/></h1>
+<h3 align="left">The Music Player is a simple desktop application that allows users to play and control music.</h3>
+<h1 align="left"></a> 
+
+<img height="32"/>Features</h1>
+<h3 align="left">Play and control music tracks.
+Skip to the next track.
+Create an account and log in to access the music player</h3>
+
+<h1 align="left">Usage</a> 
+<img height="32"/></h1>
+<h3 align="left">Click on the play button to start playing a track.
+
+Use the "<" and ">" buttons to skip the track by 10 seconds.
+
+Go to the next track by clicking the play button again.</h3>
+
+
--- login_window.py
+++ login_window.py
@@ -1,8 +1,7 @@
-import sqlite3
-from PyQt5 import QtMultimedia
+import sqlite3, pygame, csv
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QSlider, \
     QGridLayout
-from PyQt5.QtCore import Qt, QRect, QTimer, QUrl
+from PyQt5.QtCore import Qt, QRect, QTimer, QUrl, QTime
 from PyQt5.QtGui import QIcon
 
 
@@ -10,7 +9,7 @@ class LoginWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWindowTitle('Login')
-        self.setWindowIcon(QIcon('./res/tt.jpg'))
+        self.setWindowIcon(QIcon('./res/rt.png'))
         self.setGeometry(1100, 300, 400, 600)
         layout = QVBoxLayout()
         self.setLayout(layout)
@@ -73,12 +72,18 @@ class LoginWindow(QWidget):
         ans_user_in = cur.execute('''select id from logs_and_pass 
         where email = ? and pass = ?''', (self.login_ql, self.password_ql,)).fetchone()
         if ans_user_in:
+            with open('file.csv', 'a', newline='', encoding='utf8') as file:
+                writer = csv.writer(file)
+                writer.writerow(self.login_ql + ' вошел в систему.')
+
             self.hide()
             self.main_window = MainWindow()
             self.main_window.show()
         else:
             self.subheading.setText("Sorry wrong username or password.")
         con.close()
+
+
 
     def open_create_window(self):
         self.hide()
@@ -90,7 +95,7 @@ class CreateWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWindowTitle('Create account')
-        self.setWindowIcon(QIcon('./assets/lock.png'))
+        self.setWindowIcon(QIcon('./res/rt.png'))
         self.setGeometry(1100, 300, 400, 600)
 
         layout = QVBoxLayout()
@@ -157,97 +162,114 @@ class CreateWindow(QWidget):
 
 
 class MainWindow(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setWindowTitle('MainWindow')
-        # self.setWindowIcon(QIcon('./assets/lock.png'))
+        self.setWindowIcon(QIcon('./res/rt.png'))
         self.setGeometry(1100, 300, 400, 600)
         layout = QVBoxLayout()
         self.setLayout(layout)
+        box = QHBoxLayout()
+        self.subheading = QLabel(
+            '',
+            alignment=Qt.AlignmentFlag.AlignLeft
+        )
+        self.subheading.setObjectName('sub')
+
+        pygame.mixer.init()
+        self.playing = False
+        self.last_url = None
+
+        self.music_list = ['./res/O★Z - Zankoku Shangri-La.mp3', './res/BRADIO-Flyers.mp3',
+                           './res/milet — Anytime Anywhere .mp3', './res/Перемотка-Встречная.mp3',
+                           './res/Смешарики - Ветер, ветер.mp3']
+        self.current_music_index = 0
+
+        self.play = QPushButton('▶')
+        self.backward = QPushButton('<')
+        self.forward = QPushButton('>')
+
+        self.play.clicked.connect(self.playbutton)
+        self.backward.clicked.connect(self.backwardbutton)
+        self.forward.clicked.connect(self.forwardbutton)
+
+        self.lcdtimer = QTimer()
+        self.lcdtimer.timeout.connect(self.lcd_timer)
+        self.time_counter = 0
+
         self.image_layout = QHBoxLayout()
         self.label = QLabel()
         self.label.setObjectName('main_wig')
         self.label.setFixedSize(300, 300)
         self.image_layout.addWidget(self.label)
-        layout.addStretch(stretch=1)
+
+        layout.addStretch(stretch=2)
         layout.addLayout(self.image_layout)
         layout.addStretch(stretch=4)
-
-        self.dict = {
-            'D:/_Qt/EXE/Mp3/new_build/QThread_mp3/img/zvuki_prirody.mp3': []
-        }
-        self.song = ''
-
-        self.player = QtMultimedia.QMediaPlayer()
-        self.player.stateChanged.connect(self.playerState)
-
-        self.qsl = QSlider(self)
-        self.qsl.setFixedSize(350, 30)  # +++
-        self.qsl.setOrientation(Qt.Horizontal)
-
-        self.qsl.sliderMoved[int].connect(self.SetPlayPosition)
-        self.qsl.sliderReleased.connect(self.slider_released)
-
-        self.box = QGridLayout(self)
-
-        for line, song in enumerate(self.dict):
-            play_btn = QPushButton('Play', clicked=lambda ch, song=song: self.play(song))
-
-            pause_btn = QPushButton('Pause', clicked=self.pause, enabled=False)
-
-            label = QLabel(song.rsplit('/', maxsplit=1)[-1])
-            self.box.addWidget(play_btn, line, 0)
-            self.box.addWidget(pause_btn, line, 1)
-            self.box.addWidget(label, line, 2)
-
-            self.dict[song].append(play_btn)
-            self.dict[song].append(pause_btn)
-
-        self.box.addWidget(self.qsl, 2, 0, 1, 3)
-        layout.addLayout(self.box)
-        self.Play_Pause = True
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.PlayMode)
-        self.timer.start(1000)
+        box.addWidget(self.backward)
+        box.addWidget(self.play)
+        box.addWidget(self.forward)
+        layout.addWidget(self.subheading)
+        layout.addLayout(box)
+        layout.addStretch(stretch=4)
 
         self.show()
 
-    def PlayMode(self):
-        if self.Play_Pause == False:
-            self.qsl.setMinimum(0)
-            self.qsl.setMaximum(self.player.duration())
-            self.qsl.setValue(self.qsl.value() + 1000)
+    def lcd_timer(self):
+        time = QTime(self.time_counter // 3600, self.time_counter // 60, self.time_counter % 60)
+        text = time.toString('hh:mm:ss')
+        self.timer.display(text)
+        self.time_counter += 1
 
-    def slider_released(self):
-        self.player.setPosition(self.qsl.value())
+        if self.time_counter == round(self.sound.get_length()):
+            self.time_counter = 0
+            self.playing = False
+            self.last_url = None
 
-    def SetPlayPosition(self, val):
-        pass
+            self.lcdtimer.stop()
+            self.play_next_music()
 
-    def play(self, song):
-        if self.player.isAudioAvailable() == False:
-            self.player.setMedia(QtMultimedia.QMediaContent(QUrl(song)))
-            self.dict[song][1].setEnabled(True)
-            self.song = song
+    def playbutton(self):
+        if self.playing:
+            pygame.mixer.music.stop()
+        pygame.mixer.music.load(self.music_list[self.current_music_index])
+        self.last_url = self.music_list[self.current_music_index]
+        pygame.mixer.music.play()
 
-        if self.song == song:
-            pass
-        else:
-            self.player.setMedia(QtMultimedia.QMediaContent(QUrl(song)))
-            self.dict[self.song][1].setEnabled(False)
-            self.dict[song][1].setEnabled(True)
-            self.song = song
 
-        self.player.play()
-        self.Play_Pause = False
+        self.sound = pygame.mixer.Sound(self.music_list[self.current_music_index])
+        self.time_counter = 0
+        self.current_music_index += 1
+        if self.current_music_index >= len(self.music_list):
+            self.current_music_index = 0
 
-    def pause(self):
-        self.player.pause()
-        self.Play_Pause = True
+        self.subheading.setText(self.music_list[self.current_music_index -1 ][6:-4])
 
-    def playerState(self, state):
-        if state == 0:
-            self.Play_Pause = True
-            self.qsl.setSliderPosition(0)
+        self.lcdtimer.start(round(self.sound.get_length() * 1000))
+        self.playing = True
 
+    def play_next_music(self):
+        pygame.mixer.music.stop()
+        self.playbutton()
+
+    def backwardbutton(self):
+        if self.playing:
+            if self.time_counter > 10:
+                pygame.mixer.music.pause()
+                pygame.mixer.music.set_pos(self.time_counter - 10)
+                self.time_counter -= 10
+                pygame.mixer.music.unpause()
+            else:
+                pygame.mixer.music.rewind()
+                self.time_counter = 0
+
+    def forwardbutton(self):
+        if self.playing:
+            if self.time_counter + 10 < self.sound.get_length():
+                pygame.mixer.music.pause()
+                pygame.mixer.music.set_pos(self.time_counter + 10)
+                self.time_counter += 10
+                pygame.mixer.music.unpause()
+            else:
+                pass
 
